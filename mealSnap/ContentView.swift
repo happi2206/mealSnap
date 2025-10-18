7732a7741df1cd354f87b059d6e2aaa326e547c7
happi <happinessadeboye@gmail.com>
2025-10-18 14:09:04 +1100

    first commit

---
+++ mealSnap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mealSnap
+//
+//  Created by Happiness Adeboye on 18/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mealSnap/mealSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  mealSnapApp.swift
+//  mealSnap
+//
+//  Created by Happiness Adeboye on 18/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct mealSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
